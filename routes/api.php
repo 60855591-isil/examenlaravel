57448fa7a0d91a5878d75b0a0ba729786be741fa
Camila <60855591@mail.isil.pe>
2026-06-29 13:31:08 -0500

    Rutas API cambiadas al metodo explicito segun la guia

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoriasController;
 use App\Http\Controllers\Api\ProductosController;
 
-Route::apiResource('categorias', CategoriasController::class);
-Route::apiResource('productos', ProductosController::class);
+// --- RUTAS DE CATEGORÍAS ---
+Route::get('/categorias', [CategoriasController::class, 'index']);
+Route::post('/categorias', [CategoriasController::class, 'store']);
+Route::get('/categorias/{id}', [CategoriasController::class, 'show']);
+Route::put('/categorias/{id}', [CategoriasController::class, 'update']);
+Route::delete('/categorias/{id}', [CategoriasController::class, 'destroy']);
+
+// --- RUTAS DE PRODUCTOS ---
+Route::get('/productos', [ProductosController::class, 'index']);
+Route::post('/productos', [ProductosController::class, 'store']);
+Route::get('/productos/{id}', [ProductosController::class, 'show']);
+Route::put('/productos/{id}', [ProductosController::class, 'update']);
+Route::delete('/productos/{id}', [ProductosController::class, 'destroy']);
